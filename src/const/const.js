a21ee2db763223c99ab0f0963410c3fe1e0a20fe
Jorge Luis Pasten Peña <jlpp.dzx@gmail.com>
2025-05-10 01:35:57 -0600

    Update

--- src/const/const.js
+++ src/const/const.js
@@ -95,7 +95,7 @@ export const skills = [
 
 export const projects = [
   {
-    /*img: "../../../img/ecommerce-stripe-2.webp",*/
+    img: "../../../img/titleproject.jpg,
     title: "Urban Routes - Pruebas Automatizadas con Selenium y Pytest",
     link: "https://github.com/Jorge-Pasten/qa-project-Urban-Routes-es.git",
     description: `Automatización de pruebas con Selenium y Pytest para validar el flujo completo de solicitud de taxi, incluyendo selección de ruta, tarifa, servicios adicionales y pago. Cobertura de casos críticos como ingreso de dirección, validación de teléfono, tarjeta de crédito y visualización del modal de búsqueda. Código versionado con Git y ejecutable en entorno local con Python y Google Chrome.`,
@@ -118,7 +118,7 @@ export const projects = [
     ],
   },
   {
-    img: "../../../img/ecommerce-stripe-2.webp",
+    img: "../../../img/titleproject.jpg",
     title: "Urban Grocers - Pruebas Automatizadas con Pytest",
     link: "https://github.com/Jorge-Pasten/qa-project-Urban-Grocers-app-es.git",
     description: `Automatización de pruebas con Pytest y Requests para verificar la funcionalidad de creación de kits de productos en la API de Urban Grocers. Se implementaron pruebas positivas y negativas enfocadas en la validación del campo name, evaluando comportamiento ante entradas válidas, vacías, con caracteres especiales, números y tipos incorrectos. El proyecto incluye estructura modular, control de versiones con Git y ejecución local mediante Python y entorno virtual.`,
@@ -138,7 +138,7 @@ export const projects = [
     ],
   },
   {
-    img: "../../../img/ecommerce-stripe-2.webp",
+    img: "../../../img/titleproject.jpg",
     title: "Urban.Lunch - Pruebas de QA para Aplicación Móvil",
     link: "https://github.com/Jorge-Pasten/qa-project-Urban.Lunch-app.git",
     description: `Pruebas funcionales y de usabilidad en la primera versión de Urban.Lunch para Android, una app que permite pedir platillos de restaurantes locales y recogerlos en puntos designados. Se verificaron funciones clave como selección de punto de recogida, elección y confirmación de platillos, seguimiento del pedido y manejo de errores. Las pruebas se ejecutaron en emulador y dispositivo Android, con documentación en Google Docs y reporte de bugs en Jira. El trabajo permitió identificar mejoras clave para optimizar la experiencia del usuario antes del lanzamiento.`,
